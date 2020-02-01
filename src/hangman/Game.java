510c0bb1b425b41545408ccc77f8ee5e8a0b8605
Phamandy00 <Phamandy00@yahoo.com>
2020-02-01 00:24:34 -0500

    finished the game

--- src/hangman/Game.java
+++ src/hangman/Game.java
@@ -1,97 +1,127 @@
 package hangman;
+
 import java.util.ArrayList;
 import java.util.Scanner;
 
-public class Game {
+public class Game
+{
     int lives = 6;
     String word;
     ArrayList<Character> bank = new ArrayList<Character>();
-
-    Game(String randomWord) {
-        word = randomWord;
+    Game(String inputWord)
+    {
+        word = inputWord;
     }
 
-    public char inputGuess() {
+    public char inputGuess()
+    {
         Scanner scan = new Scanner(System.in);
         System.out.println("Enter a guess");
         char guess = scan.next().toLowerCase().charAt(0);
-        if (Character.isLetter(guess)) {
+        if(Character.isLetter(guess))
+        {
             return guess;
         }
-        System.out.println("Please type a valid guess");
+        System.out.println("Please type a valid letter");
         return inputGuess();
     }
 
-    public void play() {
-        System.out.println(hiddenWord() + " Guesses Left: " + lives);
-        while (lives > 0) {
+    private void addToBank(char letter)
+    {
+        bank.add(letter);
+    }
+
+    public void play()
+    {
+        System.out.println(hiddenWord() + "\nguesses left : " + lives);
+        while(lives > 0)
+        {
+            System.out.println("lives: " + lives);
             char guess = inputGuess();
-            if (checkBank(guess)) {
-                System.out.println("You already guessed '" + guess + "'.");
-            } else {
-                if (!isGuessCorrect(guess)) {
-                    if (lives == 1) {
-                        System.out.println("You Lose!");
+            if(checkBank(guess))
+            {
+                System.out.println("You have already guessed '" + guess + "'");
+            }
+            else
+            {
+                if(!isGuessCorrect(guess))
+                {
+                    if(lives == 1)
+                    {
+                        System.out.println("You lose");
                         break;
                     }
                     lives--;
                 }
                 addToBank(guess);
             }
-
-            if (isDone(hiddenWord())) {
+            if(isDone(hiddenWord()))
+            {
                 lives = 0;
-                System.out.println("You Win!");
-            } else {
-                System.out.println(hiddenWord() + " Guesses Left: " + lives);
+                System.out.println("You win!");
             }
+            else
+            {
+                System.out.println(hiddenWord() + "\nguesses left: " + lives);
+            }
+
         }
     }
 
-    private boolean isGuessCorrect(char guess) {
-        for (int i = 0; i < word.length(); i++) {
-            if (word.charAt(i) == guess) {
+    public boolean checkBank(char guess)
+    {
+        for(int i = 0; i < bank.size(); i++)
+        {
+            if(bank.get(i) == guess)
+            {
                 return true;
             }
         }
         return false;
     }
 
-    public boolean checkBank(char guess) {
-        for (int i = 0; i < bank.size(); i++) {
-            if (bank.get(i) == guess) {
+    public boolean isGuessCorrect(char guess)
+    {
+        for(int i = 0; i < word.length(); i++)
+        {
+            if(word.charAt(i) == guess)
+            {
                 return true;
             }
         }
         return false;
     }
 
-    private boolean isDone(String guessedWord) {
-        for (int i = 0; i < guessedWord.length(); i++) {
-            if (guessedWord.charAt(i) == "*".charAt(0)) {
+    private String hiddenWord()
+    {
+        String guessedWord = "";
+        for(int i = 0; i < word.length(); i++)
+        {
+            for(int j = 0; j < bank.size(); j++)
+            {
+                if(word.charAt(i) == bank.get(j))
+                {
+                    guessedWord += word.charAt(i);
+                    break;
+                }
+            }
+            if(guessedWord.length() != (i+1))
+            {
+                guessedWord += "*";
+            }
+        }
+        return guessedWord;
+    }
+
+    private boolean isDone(String guessedWord)
+    {
+        for(int i = 0; i < guessedWord.length(); i++)
+        {
+            if(guessedWord.charAt(i) == '*')
+            {
                 return false;
             }
         }
         return true;
-    }
-
-    private String hiddenWord() {
-        String guessedWord = "";
-        for (int i = 0; i < word.length(); i++) {
-            for (int j = 0; j < bank.size(); j++) {
-                if (word.charAt(i) == bank.get(j)) {
-                    guessedWord += word.charAt(i);
-                }
-            }
-            if (guessedWord.length() != (i + 1)) {
-                guessedWord += "*";
-            }
-        }
-
-        return guessedWord;
-    }
-
-    private void addToBank(char letter) {
-        bank.add(letter);
     }
 }
--- src/hangman/Main.java
+++ src/hangman/Main.java
@@ -10,16 +10,18 @@ public class Main
         RandomWordGenerator words = new RandomWordGenerator(arrWords);
         String randomWord = words.generate();
         boolean isPlaying = true;
-
         Scanner scan = new Scanner(System.in);
-
-        while (isPlaying) {
+        while(isPlaying)
+        {
             new Game(words.generate()).play();
             System.out.println("Do you want to play again?");
-            System.out.println("(y)es or (n)o?");
-            if (scan.nextLine().charAt(0) == "n".charAt(0)) {
+            System.out.println("[Y]es or [N]o");
+            if(scan.nextLine().toLowerCase().charAt(0) == 'n')
+            {
+                System.out.println("Thanks for playing!");
                 isPlaying = false;
             }
         }
     }
 }
+
